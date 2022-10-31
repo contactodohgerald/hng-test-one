87349ad7e1d2ab0bd3f0ffa8c93ab5052e617ec3
Gerald Odoh <contactodohgerald@gmail>
2022-10-30 18:50:34 -0700

    worked on the required fileds

--- src/index.js
+++ src/index.js
@@ -5,15 +5,12 @@ const port = process.env.PORT || 8000
 
 //end point for fetching users details
 app.get('/', (req, res) => {
-    const message = 'Data was successfully returned';
-    const data = {
-        slackUsername : 'xanta-codes',
-        backend : true,
-        age : 25,
-        bio : 'I`m a graduate of Microbiology with programming skills. I have been designing and creating websites since 2021. I develop web applications with tools such as NodeJs where i use framework like expressJs, adonisJs and PHP where  use framework like Laravel. Finding solutions to challenges, and focused on customer satisfaction.',
-    };
-
-    return res.set('Content-Type', 'application/json').status(200).json({message, data})
+    return res.set('Content-Type', 'application/json').status(200).json({
+      slackUsername : 'xanta-codes',
+      backend : true,
+      age : 25,
+      bio : 'I`m a graduate of Microbiology with programming skills. I have been designing and creating websites since 2021. I develop web applications with tools such as NodeJs where i use framework like expressJs, adonisJs and PHP where  use framework like Laravel. Finding solutions to challenges, and focused on customer satisfaction.'
+    })
 });
 
 app.listen(port, () => {
